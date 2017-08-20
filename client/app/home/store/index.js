6a04479df23cf9d6cf87e87d2cd9633e16cb4725
Gourav Agarwal <gourava29@gmail.com>
2017-08-20 08:06:04 +0800

    Added redux dev tool config

--- client/app/home/store/index.js
+++ client/app/home/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux';
 import reducer from '../reducers';
 
 const configureStore = (railsProps) => (
-  createStore(reducer, railsProps)
+  createStore(reducer, railsProps, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default configureStore;
