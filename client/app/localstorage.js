496603c606bdd4ec8929285046f94ca4b0fe39ae
Gourav Agarwal <gourava29@yahoo.co.in>
2017-11-02 23:37:44 +0000

    Updated seed data and added expirty for localstorage persisted state data

--- client/app/localstorage.js
+++ client/app/localstorage.js
@@ -1,7 +1,9 @@
+const persistedStateExpiry = 5*60*1000; //5min expirty
 export const loadState = () => {
     try {
-        const serializedState = localStorage.getItem("re-state");
-        if(serializedState === null)
+        const serializedState = window.localStorage.getItem("re-state");
+        const lastTimeSet = parseInt(window.localStorage.getItem("re-state-timestamp"));
+        if((new Date().getTime() - lastTimeSet > persistedStateExpiry) || serializedState === null)
             return undefined;
         return JSON.parse(serializedState);
     } catch (err) {
@@ -13,6 +15,7 @@ export const loadState = () => {
 export const saveState = (state) => {
     try {
         window.localStorage.setItem("re-state", JSON.stringify(state));
+        window.localStorage.setItem("re-state-timestamp", new Date().getTime());
     } catch (err) {
         
     }   
